1b86f1625c6ded9d98ba59c632de2fa7c944d672
SMSH_FRC <140580240+SMSHFRC@users.noreply.github.com>
2025-08-11 14:42:36 +0800

    :)))))))))))

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,20 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.FloorAlgae;
+import frc.robot.commands.Intake;
 import frc.robot.commands.LeftVisionCmd;
 import frc.robot.commands.ManualDrive;
+import frc.robot.commands.PutL1;
+import frc.robot.commands.PutL2;
+import frc.robot.commands.PutL3;
+import frc.robot.commands.PutL4;
+import frc.robot.commands.PutNet;
+import frc.robot.commands.PutProcessor;
+import frc.robot.commands.Ready;
 import frc.robot.commands.RightVisionCmd;
+import frc.robot.commands.TakeHighAlgae;
+import frc.robot.commands.TakeLowAlgae;
 import frc.robot.subsystems.LeftVisionSubsystem;
 import frc.robot.subsystems.RightVisionSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
@@ -14,6 +25,7 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ArmSubsystem;
@@ -27,13 +39,13 @@ public class RobotContainer {
   // private final Vision leftVision = new Vision();
   private final ElevatorSubsystem m_ElevatorSubsystem = new ElevatorSubsystem();
   private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();  
-  private final ArmSubsystem m_PawSubsystem = new ArmSubsystem();
+  private final ArmSubsystem m_ArmSubsystem = new ArmSubsystem();
   private final SwerveSubsystem m_SwerveSubsystem = new SwerveSubsystem();
   private final RightVisionSubsystem m_RightVisionSubsystem = new RightVisionSubsystem();
   private final LeftVisionSubsystem m_LeftVisionSubsystem = new LeftVisionSubsystem();
 
-  private final CommandJoystick button = new CommandJoystick(1);
-  private final CommandJoystick driverController = new CommandJoystick(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandJoystick buttonBoard = new CommandJoystick(OperatorConstants.kButtonBoardPort);
 
   private final SendableChooser<Command> m_chooser = AutoBuilder.buildAutoChooser();
 
@@ -44,16 +56,27 @@ public class RobotContainer {
 
   private void configureBindings() {
   //搖桿
-  BooleanSupplier ifFeed  = ()-> driverController.getRawAxis(3) <= 0.4;
+  BooleanSupplier ifFeed  = ()-> driverController.getRightTriggerAxis() > 0.5;
   DoubleSupplier xSpeedFunc = ()-> driverController.getRawAxis(1);
   DoubleSupplier ySpeedFunc = ()-> driverController.getRawAxis(0);
   DoubleSupplier zSpeedFunc = ()-> driverController.getRawAxis(4);
-  
-  driverController.button(6).whileTrue(new RightVisionCmd(m_RightVisionSubsystem, m_SwerveSubsystem, xSpeedFunc, ySpeedFunc, zSpeedFunc));
-  driverController.button(5).whileTrue(new LeftVisionCmd(m_LeftVisionSubsystem, m_SwerveSubsystem, xSpeedFunc, ySpeedFunc, zSpeedFunc));
+  driverController.leftTrigger().whileTrue(new Intake(m_IntakeSubsystem, m_ArmSubsystem, m_ElevatorSubsystem));
+  driverController.rightBumper().whileTrue(new LeftVisionCmd(m_LeftVisionSubsystem, m_SwerveSubsystem, xSpeedFunc, ySpeedFunc, zSpeedFunc));
+  driverController.leftBumper().whileTrue(new RightVisionCmd(m_RightVisionSubsystem, m_SwerveSubsystem, xSpeedFunc, ySpeedFunc, zSpeedFunc));
   m_SwerveSubsystem.setDefaultCommand(new ManualDrive(m_SwerveSubsystem, xSpeedFunc, ySpeedFunc, zSpeedFunc));
-  driverController.button(3).whileTrue(Commands.runOnce(() -> m_SwerveSubsystem.resetGyro()));
+  driverController.x().whileTrue(Commands.runOnce(() -> m_SwerveSubsystem.resetGyro()));
   //按鈕盤 
+  buttonBoard.button(0).onTrue(new PutNet(m_ArmSubsystem, m_ElevatorSubsystem, ifFeed));
+  buttonBoard.button(1).onTrue(new TakeHighAlgae(m_ArmSubsystem, m_ElevatorSubsystem));
+  buttonBoard.button(2).onTrue(new TakeLowAlgae(m_ArmSubsystem, m_ElevatorSubsystem));
+  buttonBoard.button(3).onTrue(new PutProcessor(m_ArmSubsystem, m_ElevatorSubsystem, ifFeed));
+  buttonBoard.button(4).onTrue(new FloorAlgae(m_ArmSubsystem, m_ElevatorSubsystem, ifFeed));
+  buttonBoard.button(5).onTrue(new PutL4(m_ArmSubsystem, m_ElevatorSubsystem, ifFeed));
+  buttonBoard.button(6).onTrue(new PutL3(m_ArmSubsystem, m_ElevatorSubsystem, ifFeed));
+  buttonBoard.button(7).onTrue(new PutL2(m_ArmSubsystem, m_ElevatorSubsystem, ifFeed));
+  buttonBoard.button(8).onTrue(new PutL1(m_ArmSubsystem, m_ElevatorSubsystem, ifFeed));
+  buttonBoard.button(9).onTrue(new Ready(m_ElevatorSubsystem, m_ArmSubsystem));
+
 }
 
 
